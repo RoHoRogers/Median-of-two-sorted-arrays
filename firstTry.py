a39724738fccabb31986902023f37fb66ec16857
RoHoRogers <ro-ho_rogers@hotmail.com>
2018-01-09 14:19:33 -0800

    Create firstTry.py
    first go at it

---
+++ firstTry.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        #combine both arrays
+        for i in range(len(nums2)):
+            nums1.append(nums2[i])
+    
+        #sort the current array
+        nums1.sort()
+        
+        #if even, take left/right of the middle and return sum / 2
+        if len(nums1) % 2 == 0:
+            left = nums1[(len(nums1)/2)-1]
+            right = nums1[(len(nums1)/2)]           
+            return(float((left + right))/2)            
+        #if odd, return middle
+        else:
+            return(nums1[len(nums1)/2])
+        
